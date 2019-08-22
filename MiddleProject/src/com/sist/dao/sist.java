ec1e789306babf7a7c9478cc1840f25cfd54f709
sist201 <sist201@DESKTOP-LL2EH9J>
2019-08-22 15:41:02 +0900

    김경훈

--- MiddleProject/src/com/sist/dao/sist.java
+++ MiddleProject/src/com/sist/dao/sist.java
@@ -1,7 +1,7 @@
 package com.sist.dao;
 
 public class sist {
-public static void main(String[] args) {
-	System.out.println("¾È³ç");
-}
+	public static void main(String[] args) {
+		System.out.println("¾È³ç");
+	}
 }
